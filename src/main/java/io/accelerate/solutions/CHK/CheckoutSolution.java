5bb8586c0545d9473ce2ddfec7c37b51ced2cd8a
sbx_user1051 <sbx_user1051@169.254.72.15>
2025-10-02 11:23:31 +0000

    Thu Oct 02 11:23:31 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -11,6 +11,41 @@ public class CheckoutSolution {
             'E', 40,
             'F', 10
     );
+    private static final Map<Character, Integer> SKU_TO_PRICE_2 = Map.of(
+        Map.entry('A', 50),
+        Map.entry('B', 30),
+        Map.entry('C', 20),
+        Map.entry('D', 15),
+        Map.entry('E', 40),
+        Map.entry('F', 10)
+    );
+        'A', 50,
+        'B', 30,
+        'C', 20,
+        'D', 15,
+        'E', 40,
+        'F', 10,
+        'G', 20,
+        'H', 10,
+        'I', 35,
+        'J', 60,
+        'K', 80,
+        'L', 90,
+        'M', 15,
+        'N', 40,
+        'O', 10,
+        'P', 50,
+        'Q', 30,
+        'R', 50,
+        'S', 30,
+        'T', 20,
+        'U', 40,
+        'V', 50,
+        'W', 20,
+        'X', 90,
+        'Y', 10,
+        'Z', 50
+    );
     public Integer checkout(String skus) {
         int total = 0, aCount = 0, bCount = 0, eCount = 0, fCount = 0;
         for (char c : skus.toCharArray()) {
@@ -34,7 +69,10 @@ public class CheckoutSolution {
         int initialADiscount = (aCount / 5) * 50;
         aCount -= (aCount / 5) * 5;
 
+        int fDiscount = ((fCount / 3) * 10);
+
         int discount = initialADiscount + ((aCount / 3) * 20) + ((bCount / 2) * 15) + eDiscount + ((fCount / 3) * 10);
         return total - discount;
     }
 }
+
