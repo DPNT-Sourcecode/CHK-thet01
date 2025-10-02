e2247af30d1f386d16037c88a89fd7957090e602
sbx_user1051 <sbx_user1051@169.254.72.15>
2025-10-02 10:33:33 +0000

    Thu Oct 02 10:33:33 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -1,11 +1,10 @@
 package io.accelerate.solutions.CHK;
 
-import io.accelerate.runner.SolutionNotImplementedException;
-
 public class CheckoutSolution {
     public Integer checkout(String skus) {
         System.out.println("SKUs: " + skus);
-        throw new SolutionNotImplementedException();
+        return 5;
     }
 }
+
 
