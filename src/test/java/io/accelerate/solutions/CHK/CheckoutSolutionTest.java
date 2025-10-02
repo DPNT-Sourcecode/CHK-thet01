ecb3051739897285cae46a151cad8bffe8481325
sbx_user1051 <sbx_user1051@169.254.72.15>
2025-10-02 10:32:28 +0000

    Thu Oct 02 10:32:28 UTC 2025

--- src/test/java/io/accelerate/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/io/accelerate/solutions/CHK/CheckoutSolutionTest.java
@@ -1,2 +1,23 @@
-package io.accelerate.solutions.CHK;public class CheckoutSolutionTest {
+package io.accelerate.solutions.CHK;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class CheckoutSolutionTest {
+    CheckoutSolution total;
+
+    @BeforeEach
+    public void setUp() {
+        total = new CheckoutSolution();
+    }
+
+    @Test
+    public void checkout() {
+
+        assertThat(total.checkout("123"), equalTo(123));
+    }
 }
+
