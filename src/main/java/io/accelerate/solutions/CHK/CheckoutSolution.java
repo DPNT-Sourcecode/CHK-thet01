8de22aae83e70a08bd7edccc98b344abf5210e18
sbx_user1051 <sbx_user1051@169.254.72.15>
2025-10-02 10:38:36 +0000

    Thu Oct 02 10:38:36 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -1,10 +1,26 @@
 package io.accelerate.solutions.CHK;
 
+import java.util.Map;
+
 public class CheckoutSolution {
+    private static final Map<Character, Integer> SKU_TO_PRICE = Map.of(
+            'A', 50,
+            'B', 30,
+            'C', 75,
+            'D', 3,
+            'E', 17
+    );
     public Integer checkout(String skus) {
-        System.out.println("SKUs: " + skus);
-        return 5;
+        int total = 0;
+        for (char c : skus.toCharArray()) {
+            if (!Character.isLetter(c)) {
+                continue;
+            }
+            total += (c - 'A');
+        }
+        return total;
     }
 }
+
 
 
